d6f51935cb295504e19f35803621c1757a69197d
sabin khatiwada <sabinkhatiwada59@gmail.com>
2024-08-08 07:13:00 +0545

    java pro class project

---
+++ src/Hello.java
@@ -0,0 +1,15 @@
+/*public class Hello{
+    public static void main(String[] args) { //main + cntrl space
+        System.out.println("Hello Stephen!!");
+    }
+}
+*/
+/*......Acess Modifiers....*
+*protected= same class, same package, and child class in different packages
+* default = access in class and same package
+* private = access in class only
+* static is used to manage memory i.e. memory management
+* instance depends on individuality i.e. objects
+* static depends on class
+* Scanner is used to read daat from users
+ */
